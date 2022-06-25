bb0ba7dd76e6d882e957018edca527c9e01e5586
Matt Kirby <MattJKirby@outlook.com>
2022-06-25 15:10:01 +0100

    Testing action registration

--- Api Services/CoreActionsService/app.ts
+++ Api Services/CoreActionsService/app.ts
@@ -1,10 +1,9 @@
 import express, {Application} from 'express';
-
+import { ActionRegister } from './src/actionRegistration/ActionRegister';
+import './src/coreActionLibrary/dataSourceActions/'
 
 // Server configuration
 const server:Application = express();
-
-
 
 // Express and routes configuration
 server.use(express.json());
@@ -12,11 +11,9 @@ server.use(express.json());
 
 // axios.get("http://home-service:3000/").then(res => {
 //   console.log(res.data)
-// })
+// }
 
-
-
-
+console.log(ActionRegister.GetActions()[0])
 
   server.listen(0, ():void => {
     console.log(`Service: Running here 👉 https://localhost:`);
