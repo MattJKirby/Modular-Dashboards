f26c322d39c0b56f159a61370aac887377a32d20
Matt Kirby <MattJKirby@outlook.com>
2022-07-13 14:44:05 +0100

    Added object cloning methods

--- Api Services/CoreActionsService/src/coreActionLibrary/Action.ts
+++ Api Services/CoreActionsService/src/coreActionLibrary/Action.ts
@@ -1,20 +1,28 @@
 import { ActionParameter } from "../actionParameters/ActionParameter";
 import { IActionParameter } from "../actionParameters/IActionParameter";
+import { IAction } from "./IAction";
 
-export abstract class Action {
+export abstract class Action implements IAction{
     
-    private parameters: IActionParameter<any>[] = [];
+    // public name: string
+    public parameters: IActionParameter[] = [];
+
+    constructor()
+    constructor(cloneFrom?: IAction)
+    constructor(cloneFrom?: IAction){
+        this.parameters = cloneFrom?.parameters !== undefined ? cloneFrom.parameters : []
+    }
 
     abstract execute: () => void;
 
-    getParameter<T> (parameterName: string, defaultValue: T): T {
-        
-    
-        const param = new ActionParameter<T>(parameterName,defaultValue)
-      
+    abstract clone: () => IAction;
 
-       
-        return param.value
+    getParameter<T> (parameterName: string, defaultValue: T): T {
+        const param = new ActionParameter<T>(parameterName,defaultValue)
+        this.parameters.push(param)
+        return param.Value
 
     }
+
+    
 }
--- Api Services/CoreActionsService/src/coreActionLibrary/dataSourceActions/ApiSourceAction.ts
+++ Api Services/CoreActionsService/src/coreActionLibrary/dataSourceActions/ApiSourceAction.ts
@@ -1,6 +1,8 @@
 import { ActionParameterPrimitive } from "../../parameterRegistration/ParameterTypes/ActionParameterPrimitive"
 import { ActionRegister } from "../../actionRegistration/ActionRegister"
 import { Action } from "../Action"
+import { PrimitiveTypes } from "../../parameterRegistration/ParameterTypes/PrimitiveTypes";
+import { IAction } from "../IAction";
 
 
 
@@ -8,21 +10,21 @@ import { Action } from "../Action"
 @ActionRegister.registerActionType("ApiSourceAction","/apiSource")
 export default class ApiSourceAction extends Action{
 
-    @ActionParameterPrimitive.register<string>({displayName: "my test action"})
-    public test: string = this.getParameter<typeof this.test>("test","");
+    @ActionParameterPrimitive.register(PrimitiveTypes.STRING, {displayName: "my test action"})
+    private test: string = this.getParameter<typeof this.test>("test","");
 
-    @ActionParameterPrimitive.register<boolean>({displayName: "my test action"})
-    public test2: boolean = this.getParameter<typeof this.test2>("test",false);
 
-    @ActionParameterPrimitive.register<boolean>({displayName: "my test action"})
-    public test3: boolean = this.getParameter<typeof this.test3>("test",false);
-
-    constructor(){
-        super()
-    }
-    
     execute = (): void => {
 
     }
+
+
+    constructor(clone?: IAction){
+        super(clone)
+        console.log(true, clone)
+    }
+
+ 
+    override clone = ():  IAction => new ApiSourceAction(this)
 
 }
