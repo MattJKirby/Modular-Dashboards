b2b61270f0baeb295a1add981c5a61b7c22cd9f7
Matt Kirby <MattJKirby@outlook.com>
2022-07-22 00:31:05 +0100

    Action register now does not handle parameters

--- Api Services/CoreActionsService/src/actionRegistration/ActionRegister.ts
+++ Api Services/CoreActionsService/src/actionRegistration/ActionRegister.ts
@@ -1,9 +1,9 @@
 
 import { IAction } from "../coreActionLibrary/IAction";
-import { RegisteredActionParameter } from "../parameterRegistration/RegisteredActionParameter";
+import { ActionParameterTemplate } from "../Templating/ParameterTemplating/ActionParameterTemplate";
+import { Templator } from "../Templating/Templator";
 import { ActionRegistrationFactory } from "./ActionRegistrationFactory";
-import { IRegisteredAction } from "./IRegisteredAction";
-
+import { RegisteredAction } from "./RegisteredAction";
 
 /**
  * Stores a list of all action types, ensuring they contains all info for successful registration.
@@ -13,12 +13,7 @@ export namespace ActionRegister {
     /**
      * Maintain a list of all registered actions
      */
-     const registeredActions: IRegisteredAction<ActionConstructor<IAction>>[] = [];
-
-    /**
-     * List of all the parameters pertaining to the registered action.
-     */
-    const actionParameters: RegisteredActionParameter<any>[] = [];
+     const registeredActions: RegisteredAction<ActionConstructor<IAction>>[] = [];
     
     /**
      * Readonly action constructor type using generic
@@ -34,27 +29,21 @@ export namespace ActionRegister {
      * @param endpoint 
      * @returns 
      */
-    export function registerActionType<ActionType extends ActionConstructor<IAction>> (typeName: string, endpoint: string) {
+    export function registerAction<ActionType extends ActionConstructor<IAction>> (typeName: string, endpoint: string) {
         return (ctor: ActionType) => {
             const newRegistration = ActionRegistrationFactory.newRegistration<ActionType>(typeName, ctor, endpoint);
-            newRegistration.registeredParameters = actionParameters.splice(0, actionParameters.length);
+            const templator = Templator.getTemplator(newRegistration.TypeName)
+            console.log(templator.find(ActionParameterTemplate))
             registeredActions.push(newRegistration);
         }
     }
 
-    /**
-     * Registers action parameters
-     * @param parameter 
-     */
-    export const registerActionParameter = (parameter: RegisteredActionParameter<any>) => {
-        actionParameters.push(parameter)
-    }
 
     /**
      * Obtains a list of all registered actions
      * @returns 
      */
-    export const GetActions = (): IRegisteredAction<ActionConstructor<IAction>>[] => {
+    export const GetActions = (): RegisteredAction<ActionConstructor<IAction>>[] => {
         return registeredActions;
     }
 }
